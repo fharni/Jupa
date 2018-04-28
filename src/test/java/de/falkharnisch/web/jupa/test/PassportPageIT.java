6ddcbbdc60ef08080e735586de59bc9b060fb756
aharnisch <avenger_i-github@yahoo.de>
2018-04-28 18:30:20 +0200

    Ŕename PassportPageTest

--- src/test/java/de/falkharnisch/web/jupa/test/PassportPageIT.java
+++ src/test/java/de/falkharnisch/web/jupa/test/PassportPageIT.java
@@ -2,8 +2,6 @@ package de.falkharnisch.web.jupa.test;
 
 import static org.testng.Assert.assertEquals;
 
-import java.util.concurrent.TimeUnit;
-
 import org.jboss.arquillian.graphene.page.InitialPage;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.testng.Arquillian;
@@ -13,7 +11,7 @@ import de.falkharnisch.web.jupa.page.LoginPage;
 import de.falkharnisch.web.jupa.page.MenuHeader;
 import de.falkharnisch.web.jupa.page.PassportPage;
 
-public class PassportPageTest extends AbstractArquillianTest {
+public class PassportPageIT extends AbstractArquillianTest {
 
 	@Page
 	private MenuHeader menuHeader;
@@ -29,11 +27,10 @@ public class PassportPageTest extends AbstractArquillianTest {
 
 		assertEquals(passportPage.getUsername(), "0502001000015", "Username doesn't match");
 		// Wenn Encoding Problem gelöst ist die nachfolgende Zeile reaktivieren
-		// assertEquals(passportPage.getName(), "Schüler Eins", "Name doesn't match");
+//		assertEquals(passportPage.getName(), "Schüler Eins", "Name doesn't match");
 		assertEquals(passportPage.getClubName(), "TV 1875 Paderborn e.V.", "Clubname doesn't match");
 		assertEquals(passportPage.getDistrictName(), "Bielefeld", "Districtname doesn't match");
 		assertEquals(passportPage.getFederationName(), "Nordrhein-Westfalen", "Federationname doesn't match");
-		TimeUnit.SECONDS.sleep(5);
 	}
 
 }
