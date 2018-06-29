ec5cd1d8e1737332c670dcbec85970882065ecf0
Falk Harnisch <github@falkharnisch.de>
2018-06-29 16:05:02 +0200

    add testdata for course

---
+++ docker/init-data/T12_audit_testdaten.sql
@@ -0,0 +1,5 @@
+INSERT INTO audit (id, club_id, auditor_id, startdate, status_id) VALUES (1, 1, 1, '2018-07-04 20:30:00', 1);
+INSERT INTO audit (id, club_id, auditor_id, startdate, status_id) VALUES (2, 1, 4, '2018-07-07 08:30:00', 1);
+INSERT INTO audit (id, club_id, auditor_id, startdate, status_id) VALUES (3, 2, 2, '2018-03-10 12:30:00', 1);
+INSERT INTO audit (id, club_id, auditor_id, startdate, status_id) VALUES (4, 2, 5, '2018-07-07 09:00:00', 1);
+INSERT INTO audit (id, club_id, auditor_id, startdate, status_id) VALUES (5, 3, 4, '2018-06-30 10:00:00', 1);
