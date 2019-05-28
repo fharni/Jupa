5eb888b07c3b093f9b09a1cc7bdc964d35d48a48
fharni <github@falkharnisch.de>
2019-05-28 20:35:57 +0200

    update Version of CDI and JSF

---
+++ src/main/java/de/falkharnisch/web/jupa/beans/ConfigurationBean.java
@@ -0,0 +1,10 @@
+package de.falkharnisch.web.jupa.beans;
+
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig(
+        // Activates CDI build-in beans
+        version = FacesConfig.Version.JSF_2_3)
+public class ConfigurationBean {
+
+}
